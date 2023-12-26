8b8c5fb1f471274ce829f7ae218f3e1ef7dafab8
Abdulramon Sodiq <sodiqramon0@gmail.com>
2023-12-26 10:01:27 +0000

    First commit

---
+++ jenny_lecture/1_intro.c
@@ -0,0 +1,24 @@
+/**
+ * -> Structure of C programming
+ *
+ *  * Documentation section - A comment section that include the author, date, and description of the program.
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: return 0 (success)
+ */
+int main(void)
+{
+	int a, b;
+
+	a = 30;
+	b = 15;
+
+	printf("%d\n", 30 + 15);
+
+	return (0);
+}
