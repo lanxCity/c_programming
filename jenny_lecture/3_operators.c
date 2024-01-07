1796baa268ebb9e206eced90691c116d8807248f
Abdulramon Sodiq <sodiqramon0@gmail.com>
2024-01-07 14:54:13 +0000

    Miscellaneous operator

--- jenny_lecture/3_operators.c
+++ jenny_lecture/3_operators.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 
-	int a, b, c, d, e, f;
+	int a, b, c, d, e, f, g;
 
 	/*-----> increament and decreament*
 
@@ -64,6 +64,7 @@ int main(void)
 	a = 18, b = 10;
 	c = 9, d = 10;
 	e = 10, f = 1;
+	g = 4, f = 12;
 
 	printf("The relation is: %d\n", a + c == b >= e < c + d != 1);
 
@@ -71,7 +72,39 @@ int main(void)
 
 	printf("Bitwise AND (&): %d & %d -> %d\n", b, c, b & c);
 	printf("Bitwise OR (|): %d | %d -> %d\n", b, c, b | c);
-	printf("Bitwise AND (^): %d & %d -> %d\n", b, c, b ^ c);
+	printf("Bitwise XOR (^): %d ^ %d -> %d\n", b, c, b ^ c);
+	printf("Bitwise Left shift (<<): %d << %d -> %d\n", b, g, b << g);  // b * (2^3); dealing with binary
+	printf("Bitwise Right shift (>>): %d >> %d -> %d\n", b, g, b >> g);  // b / (2^3); dealing with binary
+	printf("Bitwise Right shift (~): ~%d -> %d\n\n", f, ~f);
+
+	/*------> Special characters*/
+	printf("------------------> The comma operator\n");
+	
+	a = (printf("I am inside comma operator\n"), 35, 90);
+	b = 67, 12, 3, 43;
+
+	printf("The value of a: %d\n", a);
+	printf("The value of b: %d\n", b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
