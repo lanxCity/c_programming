e68d3cff102f6dd88c2adbbec84a79f42ddf6fec
Abdulramon Sodiq <sodiqramon0@gmail.com>
2024-01-10 08:08:54 +0000

    Repaeration

--- jenny_lecture/4_input_output.c
+++ jenny_lecture/4_input_output.c
@@ -3,7 +3,6 @@
 /**
  * main - Entry point
  *
-<<<<<<< HEAD
  * Result: success (0)
  */
 int main()
@@ -13,14 +12,4 @@ int main()
 	printf("---------> Formatted input \"scanf()\" function\n");
 
 
-=======
- * Return: success (0)
- */
-int main(void)
-{
-
-	/*----------> Formatted input "scanf()" ---------*/
->>>>>>> e8831300e8671da47506b7837b82178cc8f76f92
-
-	return (0);
 }
