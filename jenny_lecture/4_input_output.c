f2935e24a167736bc57da48101986a3b6478dbf1
Abdulramon Sodiq <Sodiqramon0@gmail.com>
2024-01-10 07:59:31 +0000

    My commit

---
+++ jenny_lecture/4_input_output.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Result: success (0)
+ */
+int main()
+{
+	/*---------> Formatted input "scanf()"*/
+
+	printf("---------> Formatted input \"scanf()\" function\n");
+
+
+
+	return (0);
+}
