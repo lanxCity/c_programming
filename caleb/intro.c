823946279deba0a3592b3fd5554100448899316b
Abdulramon Sodiq <sodiqramon0@gmail.com>
2024-03-06 07:13:21 +0100

    Starting up the tutorial

---
+++ caleb/intro.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Code Entry point function
+ * Return: 0 (success)
+ */
+int main()
+{
+    /**-> Variables
+     * - The declaration of a variable is called "Variable Decoration"
+     * - Assignment of a value to the variable declared is called "variable initialization"
+     */
+    int x;
+
+    return (0);
+}
