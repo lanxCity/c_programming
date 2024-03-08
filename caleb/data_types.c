b268899ff514f3ce1218feebe1a66c0c65010ee5
Abdulramon Sodiq <sodiqramon0@gmail.com>
2024-03-08 09:46:05 +0000

    Done with numeric data type

--- caleb/data_types.c
+++ caleb/data_types.c
@@ -7,12 +7,77 @@
 int main()
 {
 	/**
-	 * ->  
+	 * -> Numeric data type
+	 *  - int: these are whole numbers
+	 *  - floating point number: these are numbers with fractional part and it's includes
+	 *    float and double.
+	 *   * The difference between the float and double is precision
 	 */
 
+	/**float dec1 = 11111.11111;
+	float dec2 = 11111.11111F;
+
+	/*---These prints the same outputs* 
+	printf("dec1: %f\n", dec1);
+	printf("dec2: %f\n", dec2);
+	*/
+
+	/*This prints different outputs*/
+	/*It is an evidence that the value itself are basically considered "const" and not the data type*/
+	/*The floating point values are "doubled" by standard*/
+	/*printf("dec1: %f\n dec2: %f\n", 11111.11111, 11111.111111F);*/
+
+	/**---> Scientific notation of numeric values
+	 * This is when a number is converted to the power of its decimal e.g. 3567 ~= 3.567 * 10^3
+	 * we have format as "%e" and "%g", where g is to let the compiler decides when sci-notation is necessary
+	 * The format are originally used floating numbers while printing it.
+	 *
+	 * The power <= -4 or power <= 5 are not converted to scientic notation by defaul
+	 * 
+	 * NB: if you wanna escape "%", you double it as "%%".
+	 */
+
+	int a, b;
+
+	a = 2.542e3;
+	b = 25.7e2;  /*multiply by 10^3*/
+
+	printf("-------------> INTEGERS\n");
+	printf("=> %i : %i\n", a, a);
+	printf("=> %i : %i\n", b, b);
+
+	double c, d;
+	
+	c = 2.542e9;
+	d = 25.7e3;  /*multiply by 10^3*/
+
+	printf("-------------> FLOATING\n");
+	printf("=> %f (%%e): %e\n", c, c);
+	printf("=> %f (%%g): %g\n", c, c);
+	
+	printf("=> %f (%%e): %e\n", d, d);
+	printf("=> %f (%%g): %g\n", d, d);
+	
+	/*To read sci-notation from stdin, we use "lf" and also works for decimal*/
+	printf("Enter sci-notation value: ");
+	scanf("%lf", &d);
+
+	printf("=> %f (%%e): %e\n", d, d);
+	printf("=> %f (%%g): %g\n", d, d);
 
 
 	return (0);
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
--- caleb/intro.c
+++ caleb/intro.c
@@ -45,7 +45,7 @@ int main()
 
     printf("-> %i / %i is: %f\n\n", sum, num, div);
 
-    /*-> Strings*
+    /**-> Strings*
     char lastName[31];
     char firstName[31];
 
@@ -62,5 +62,86 @@ int main()
      * - passing of the argument allows to change the behaviour of a function
      */
 
+    /**-> Constant in C programming
+     * - Generally, a constant is a variable that its value can never be changed e.g. PI.
+     * - However, basically in C, a constant is just a value assigned to a variable of a certain data type e.g. 
+     *   int num = 7. In this case, 7 is a numerical (decimal) constant but the value can be changed later.
+     *
+     * - In the case of data type constant, Consider the following example,
+     *
+     * In C programming, when you declare a floating-point constant, you can use either the f suffix 
+     * or the decimal point to specify that the value is a float. Lets break down the difference:
+     ** Given 3.0F and 3.0
+     * The 3.0f represents a floating-point literal with the value 3.0.
+     * The f suffix explicitly indicates that this value is a single-precision float.
+     * It occupies 32 bits of memory.
+     * Example usage: float myFloat = 3.0f;
+     *
+     * The 3.0 (without the f suffix) is also a floating-point literal.
+     * However, it is treated as a double-precision float by default.
+     * A double uses 64 bits of memory.
+     * Example usage: double myDouble = 3.0;
+     *
+     * Result Differences:
+     * When performing arithmetic operations, the different precisions can lead to varying results.
+     * For example, multiplying 97346822 by 3.0f yields 292040480.0, while multiplying by 3.0 gives 292040466.0.
+     * The discrepancy arises due to the internal representation of these values in memory.
+     * Remember that specifying 3.0f ensures that youre working with a float, while 3.0 defaults to a double.
+     *
+     *
+     *
+     * - Symbolic Constants: Provide a way to give a name to a constant value. In this case, the value of 
+     *   the variable remains constant throughout the program.
+     *
+     * a. Using #define:
+     *    #define PI 3.14
+     *
+     * b. Using const:
+     *    const double PI = 3.14
+     */
+
+     float a = 3.0;
+     float b = 3.0F;
+
+     int c = 97346822;
+
+     /*Same result since a and b are defined as float but some times, one might want to be*/
+     /*specific about the type of constant*/
+     printf("=> %f x %d: %f\n", a, c, a * c); 
+     printf("=> %f x %d: %f\n", b, c, b * c);
+
+
+
+
+
+
+
+
+
+
+    
+    
+    	
+
+
     return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
